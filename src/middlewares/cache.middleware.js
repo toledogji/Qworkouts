f13e568687473062838e7a376dc8a1e6416403be
toledogji <toledogji@gmail.com>
2020-04-02 18:27:07 -0300

    [ADD] Cache added.

---
+++ src/middlewares/cache.middleware.js
@@ -0,0 +1,22 @@
+const memcache = require('memory-cache');
+const { CACHE_KEY } = require('../config');
+
+module.exports = function(duration){
+
+    return (req, res, next) => {
+        const key = CACHE_KEY + req.originUrl || req.url;
+        const cachedBody = memcache.get(key);
+        
+        if(cachedBody){
+            return res.send(JSON.parse(cachedBody));
+        }else{
+            res.sendResponse = res.send;
+            res.send = body => {
+                memcache.put(key, body, duration * 100);
+                res.sendResponse(body);
+            };
+            next();
+        }
+    };
+
+};
